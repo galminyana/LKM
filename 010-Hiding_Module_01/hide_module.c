32853430a4e39d5deb106bb7fbd6498f6040daeb
Guillem <46190684+galminyana@users.noreply.github.com>
2021-03-10 23:31:04 +0100

    Update hide_module.c

--- 010-Hiding_Module_01/hide_module.c
+++ 010-Hiding_Module_01/hide_module.c
@@ -1,19 +1,30 @@
 #include <linux/module.h>
 #include <linux/init.h>
 
+/*
+ Will store the head for the list where THIS_MODULE is 
+*/
+struct list_head  * module;
+
 static int __init lkm_init(void)
 {
-        pr_info("\nHiding Module...\n");
+        module = THIS_MODULE->list.prev;                //<- Save list head for this module
+        
+        pr_info("\nHiding Module\n");
 
-        pr_info("  -- Hiding from lsmod...\n");
-
+        pr_info("  -- Hiding from lsmod\n");
+        
         list_del_init(&__this_module.list);
 
-        pr_info("  -- Hiding for sysfs...\n");
+        pr_info("  -- Showing for lsmod and /proc\n");
+
+        list_add(&__this_module.list, module );
+        
+        pr_info("  -- Hiding for sysfs\n");
 
         kobject_del(&(&__this_module)->mkobj.kobj);
 
-        pr_info("Module Hided!\n");
+        pr_info("Done!\n");
 
         return 0;
 }
@@ -29,6 +40,6 @@ module_exit(lkm_exit);
 
 MODULE_AUTHOR("Guillem Alminyana");
 MODULE_LICENSE("GPL v2");
-MODULE_DESCRIPTION("LKM Hide Module Example");
+MODULE_DESCRIPTION("LKM Hide/Show Module Example");
 ~                                                                                                                    
 ~                       
