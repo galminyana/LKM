82f9585f384bb764bab0f23600d09d4437e6998d
Guillem <46190684+galminyana@users.noreply.github.com>
2021-03-21 00:38:56 +0100

    Update mutex.c

--- 008-Mutex/mutex.c
+++ 008-Mutex/mutex.c
@@ -1,23 +1,44 @@
-
-#include "my_lkm_lib.h"			//<- Own lib
 #include <linux/module.h>
 #include <linux/init.h>
 
+static DEFINE_MUTEX(my_mutex);
 
 static int __init lkm_init(void)
 {
-	
-	return 0;
+        pr_info("Loading Module\n");
+
+        pr_info("   Initializing mutex..\n");
+        mutex_init(&my_mutex);          //<- Mutex initialization
+        pr_info("   Mutex Initialized.\n");
+
+        pr_info("   mutex_is_locked: %d\n.", mutex_is_locked(&my_mutex));
+
+        pr_info("   Locking Mutex...\n");
+        mutex_lock(&my_mutex);
+        pr_info("   Mutex Locked.\n");
+
+        pr_info("   mutex_is_locked: %d\n.", mutex_is_locked(&my_mutex));
+
+        pr_info("   Releasing mutex...");
+        mutex_unlock(&my_mutex);
+        pr_info("   Mutex unlocked.\n");
+
+	pr_info("   mutex_is_locked: %d\n.", mutex_is_locked(&my_mutex));
+
+        return 0;
 }
 
 static void __exit lkm_exit(void)
 {
-        pr_info("Exiting... Bye World\n");
+        pr_info("Destroying Mutex.\n");
+        mutex_destroy(&my_mutex);
+
+        pr_info("Exiting... \n");
 }
 
-module_init(lkm_init);          									//<- Tells that Init Kernel Module is the lkm_init function
-module_exit(lkm_exit);          									//<- Tells that Exit Kernel Function is lkm_exit
+module_init(lkm_init);          //<- Tells that Init Kernel Module is the lkm_init function
+module_exit(lkm_exit);          //<- Tells that Exit Kernel Function is lkm_exit
 
 MODULE_AUTHOR("Guillem Alminyana");
-MODULE_LICENSE("GPL v2");
+MODULE_LICENSE("GPL");
 MODULE_DESCRIPTION("LKM Mutex Example");
