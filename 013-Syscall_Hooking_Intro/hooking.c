f2aa04e82fb992b1664dd919656645a3af6545c7
Guillem <46190684+galminyana@users.noreply.github.com>
2021-03-23 23:52:14 +0100

    Update hooking.c

--- 013-Syscall_Hooking_Intro/hooking.c
+++ 013-Syscall_Hooking_Intro/hooking.c
@@ -1,55 +1,58 @@
+#include <linux/init.h>
 #include <linux/module.h>
 #include <linux/kernel.h>
-#include <linux/kallsyms.h>
 #include <linux/syscalls.h>
+#include <linux/kallsyms.h>
 
 #include "my_memory.h"
 
-static unsigned long *syscall_table;                            //<- Pointer to the Kernel syscall table
+static unsigned long *__sys_call_table;				//<- Pointer to the Kernel syscall table
 
-asmlinkage int (*original_shutdown) (int, int);                 //<- Define the original syscall
+//asmlinkage int (*original_shutdown) (int, int);
+asmlinkage int (*original_mkdir) (const char * path);
 
 
-asmlinkage int hooked_shutdown(int i, int z)
+asmlinkage int hooked_mkdir(const char * path)
 {
-        pr_info("   Hooked Syscall!!\n");
-        //return orig_shutdown(i, z);
-        return 0;
+	pr_info("   Hooked Syscall!!\n");
+	//return orig_shutdown(i, z);
+	return 0;
 }
+
 
 static int __init lkm_init(void)
 {
         pr_info("Loading Module...\n");
 
-        syscall_table = (void *) kallsyms_lookup_name("sys_call_table");                //<- Get address for Kernel Syscall Table
+  	__sys_call_table = (void *) kallsyms_lookup_name("sys_call_table");		//<- Get address for Kernel Syscall Table
+ 
+  	if (__sys_call_table == NULL) {
+		pr_err("  Can't find syscall table!.\n");
+    		return -1;
+  	}
 
-        if (sys_call_table == NULL) {
-                pr_err("   Can't find syscall table!.\n");
-                return -1;
-        }
+	original_mkdir = (void *) __sys_call_table[__NR_mkdir];	//<- Save actual address for sys_shutdown
 
-        original_shutdown = (void *) syscall_table[__NR_shutdown];      //<- Save actual address for sys_shutdown
+	my_memory_rw();					//<- Unprotect Memory
 
-        my_memory_rw();                                 //<- Unprotect Memory
+	__sys_call_table[__NR_mkdir] = (unsigned long) hooked_mkdir;
 
-        pr_info("   Hijacking Syscall Table.\n");
-        syscall_table[__NR_shutdown] = (unsigned long) hooked_shutdown;
-
-        my_memory_ro();
+	my_memory_ro();
 
         return 0;
 }
 
+
 static void __exit lkm_exit(void)
 {
-        my_memory_rw();
-
-        pr_info("   Restoring original syscall.\n");        
-        syscall_table[__NR_shutdown] = (unsigned long) original_shutdown;
-
-        my_memory_ro();
-
         pr_info("Exiting... Bye.\n");
+
+	my_memory_rw();
+
+	__sys_call_table[__NR_mkdir] = (unsigned long) original_mkdir;
+
+	my_memory_ro();
+
 }
 
 module_init(lkm_init);           
