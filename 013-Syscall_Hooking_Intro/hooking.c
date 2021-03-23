ac906578b1c6729e1cede75b89e83b137a85a2f2
Guillem <46190684+galminyana@users.noreply.github.com>
2021-03-23 22:53:54 +0100

    Update hooking.c

--- 013-Syscall_Hooking_Intro/hooking.c
+++ 013-Syscall_Hooking_Intro/hooking.c
@@ -52,8 +52,8 @@ static void __exit lkm_exit(void)
         pr_info("Exiting... Bye.\n");
 }
 
-module_init(lkm_init);           //<- Tells that Init Kernel Module is the lkm_init function
-module_exit(lkm_exit);          //<- Tells that Exit Kernel Function is lkm_exit
+module_init(lkm_init);           
+module_exit(lkm_exit);          
 
 MODULE_AUTHOR("Guillem Alminyana");
 MODULE_LICENSE("GPL");
