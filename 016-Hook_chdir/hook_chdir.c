8c34798003a3c182eaf7376b81f84922935b7376
Guillem <46190684+galminyana@users.noreply.github.com>
2021-03-30 22:56:14 +0200

    Update hook_chdir.c

--- 016-Hook_chdir/hook_chdir.c
+++ 016-Hook_chdir/hook_chdir.c
@@ -8,25 +8,40 @@
 
 #include "my_memory.h"
 
-static unsigned long *__sys_call_table;                         //<- Pointer to the Kernel syscall table
+static unsigned long *__sys_call_table;                             //<- Pointer to the Kernel syscall table
 
 asmlinkage long (*original_chdir) (const struct pt_regs *regs);     //<- Original getdents64 definition
 
 asmlinkage long hooked_chdir (const struct pt_regs *regs)
 {
-        char __user * file_user = (char *) regs->di;                    //get the pointer to the string
-        char * file_kern = NULL;
+        char __user * file_user = (char *) regs->di;                //<- Pointer to the Path String in User Space
+        char * file_kern = NULL;                                    //<- Pointer to the path string in Kernel Space
+        int length;                                                 //<- Length of the path string
+        int value;                                                  //<- for error checking
 
-        int value = copy_from_user(file_kern, file_user, sizeof(file_user));
+        length = strlen(file_user);                                 //<- Path total length
 
-        if (value) {
-                pr_err("   Error copying from user space.\n");
+        file_kern = kzalloc(length, GFP_KERNEL);                    //<- Allocate memory to store the path in kernel space
+        
+        if (file_kern == NULL) {                                    //<- Error Checking
+                pr_err("   Error allocating space for path.\n");
                 return -EINTR;
         }
 
+        value = copy_from_user(file_kern, file_user, strlen(file_user));   //<- Copy the path from User to Kernel Space
+
+        if (value) {                                                //<- Error Checking
+                pr_err("   Error copying from user space.\n");
+                return -EINTR;
+        }
+        
         pr_info("   File is: %s.\n", file_kern);
 
-        return original_chdir(regs);
+        kfree(file_kern);                                           //<- Release allocated Kernel Space
+
+        return original_chdir(regs);                                //<- Call to original chdir syscall
+        //return 0;
+
 }
 
 static int __init lkm_init(void)
@@ -40,11 +55,11 @@ static int __init lkm_init(void)
                 return -1;
         }
 
-        original_chdir = (void *) __sys_call_table[__NR_chdir];       //<- Save actual address for sys_shutdown
+        original_chdir = (void *) __sys_call_table[__NR_chdir];                 //<- Save actual address for sys_shutdown
 
         my_memory_rw();
 
-        __sys_call_table[__NR_chdir] = (unsigned long) hooked_chdir;   //<- Replace the syscall pointer in the syscall table
+        __sys_call_table[__NR_chdir] = (unsigned long) hooked_chdir;            //<- Replace the syscall pointer in the syscall table
 
         my_memory_ro();
 
@@ -57,7 +72,7 @@ static void __exit lkm_exit(void)
 
         my_memory_rw();
 
-        __sys_call_table[__NR_chdir] = (unsigned long) original_chdir;
+        __sys_call_table[__NR_chdir] = (unsigned long) original_chdir;          //<- Restore syscall
 
         my_memory_ro();
 
