07dc7f19a4e23f12871773fccc24f63d74b788dc
Guillem <46190684+galminyana@users.noreply.github.com>
2021-04-11 21:04:50 +0200

    Update scheduler_lkm.c

--- 009-Task_scheduling/scheduler_lkm.c
+++ 009-Task_scheduling/scheduler_lkm.c
@@ -5,33 +5,45 @@
 #include <linux/sched.h>
 #include <linux/interrupt.h>
 
-static int times_task_executed = 0;                          //<- To hold hoy many times task been executed
+static int times_task_executed = 0;                         //<- To hold hoy many times task been executed
 
 static void scheduled_task (struct work_struct * work);     //<- Function prototipe definition
 
+static int stop = 0;                                        //<- When "1", stop running the task
+
 static struct workqueue_struct * my_workqueue;
-static struct work_struct my_task;
 
-static DECLARE_WORK(my_task, scheduled_task);               //<- Declare and assign the function to the task
-
+static DECLARE_DELAYED_WORK(my_task, scheduled_task);       //<- Declare and assign the function to the task
+                                                            //   Defines a work_struct called my_task
 static void scheduled_task (struct work_struct * work)
 {
         pr_info("   Executed task %d times.\n", times_task_executed);
 
         times_task_executed++;
 
+        if (stop == 0)                                      //<- Still don't stop
+        {
+                queue_delayed_work(my_workqueue, &my_task, 100);
+        }
 }
 
 static int __init lkm_init (void)
 {
-
+        pr_info("Loading Module.\n");
+        
+        my_workqueue = create_workqueue("my_Queue_name");
+        queue_delayed_work(my_workqueue, &my_task, 100);
 
         return 0;
 }
 
 static void __exit lkm_exit (void)
 {
+        stop = 1;                        //<- Tell the qork queue to stop execution
 
+        cancel_delayed_work(&my_task);
+        flush_workqueue(my_workqueue);
+        destroy_workqueue(my_workqueue);
 
         pr_info("Exiting... \n");
 }
